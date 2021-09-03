e4ff06558ec665455668bd9764d2f08ba7c0975d
saraivadev <wsaraiva@outlook.com>
2021-09-03 06:43:28 -0300

    firts commit d

--- product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/config/ElasticSearchConfig.java
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/config/ElasticSearchConfig.java
@@ -2,7 +2,6 @@ package one.digitalinnovation.experts.productcatalog.config;
 
 
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -16,7 +15,6 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
     @Override
-    @Bean
     public RestHighLevelClient elasticsearchClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo("localhost:9200", "localhost:9300")
---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/model/Product.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation.experts.productcatalog.model;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "product")
+public class Product {
+    private Long id;
+    private String name;
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
